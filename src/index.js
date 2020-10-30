22ae3d32aeb650c67da404fb944f08a175b5b85d
Charles Pletcher <pletcher@deleuze.local>
2020-10-30 15:38:45 -0400

    Remove GreekProsody
    It's not really a great demonstration of what Draft.js does.
    
    In fact, Draft.js isn't really needed for rendering this stuff/handling
    these interactions.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import Annotatable from './components/Annotatable'
 import AnnotationEditor from './components/AnnotationEditor'
 import Annotation from './decorators/Annotation'
-import GreekProsody from './decorators/GreekProsody'
 
 export * from './components/Annotatable'
 export * from './components/Annotation'
 export * from './components/AnnotationEditor'
 export * from './decorators/Annotation'
-export * from './decorators/GreekProsody'
 
-export { Annotation, AnnotationEditor, GreekProsody }
+export { Annotation, AnnotationEditor }
 export default Annotatable
 
